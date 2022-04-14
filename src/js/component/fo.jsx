484403d75820fcbc66333aed951711c05e2fedce
jorgerebolledoa <aziel.az18@gmail.com>
2022-04-14 19:14:50 +0000

    exercise-landing-page-with-react

---
+++ src/js/component/fo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Fo = () => {
+	return (
+		<div className="p-3  bg-dark  text-center">
+			<div className="text-white">copyright</div>
+		</div>
+	);
+};
+export default Fo;
