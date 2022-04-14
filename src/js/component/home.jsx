92dc9828facf8398dee85d6e1ff9058c9a8bd1dd
jorgerebolledoa <aziel.az18@gmail.com>
2022-04-14 22:09:01 +0000

    exercise-landing-page-with-react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,7 +13,14 @@ const Home = () => {
 	return (
 		<>
 			<Nav title="StartBooststrap" />
-			<Jumbotron />
+			<Jumbotron
+				title="Custom jumbotron"
+				text="Using a series of utilities, you can create this jumbotron,
+			just like the one in previous versions of Bootstrap. Check
+			out the examples below for how you can remix and restyle it
+			to your liking."
+				textbtn="Example button"
+			/>
 			<Cards />
 			<Footer title="Cop" />
 		</>
